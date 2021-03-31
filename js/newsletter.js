890c6f24740541238b8c88b429f2e6d4b3587eba
Kim S <kirasamp@gmail.com>
2021-03-31 12:18:26 +0200

    don't ignore js files

---
+++ js/newsletter.js
@@ -0,0 +1,33 @@
+const overlay = document.querySelector(".overlay");
+const popup = document.querySelector(".newsletter");
+const title = document.querySelector(".popup-title");
+const emailInput = document.getElementById("email");
+const emailForm = document.querySelector(".newsletter-form");
+const btnClosePopup = document.querySelector(".close-newsletter");
+const btnSignup = document.querySelector(".sign-up");
+
+const emailList = [];
+
+// Close Popup
+const closePopup = function () {
+  popup.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+overlay.addEventListener("click", closePopup);
+
+btnClosePopup.addEventListener("click", closePopup);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !popup.classList.contains("hidden")) {
+    closePopup();
+  }
+});
+
+// Submit Email
+btnSignup.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  emailList.push(emailInput.value);
+  emailForm.classList.add("hidden");
+  title.textContent = "Thank you for signing up!";
+});
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+const App = (() => {
+
+  const init = () => {
+    // const header = document.querySelector('.layout-header');
+    // const imageContainer = document.querySelectorAll('.image-container');
+    // const carouselSlider = document.querySelector('.carousel-slider');
+
+    // if(header) {
+    //   HeaderHandler(header, document.body);
+    // }
+
+    // if(imageContainer.length > 0) {
+    //   for(let i = 0; i < imageContainer.length; i++) {
+    //     LazyLoad(imageContainer[i]);
+    //   }
+    // }
+
+    // if(carouselSlider) {
+    //   CarouselSlider(carouselSlider);
+    // }
+  };
+
+  init();
+})();
