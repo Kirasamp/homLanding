2777c7cf5f619f091d72c239b95621851767a88d
AllanBerlin <allanfitzpatrick9@gmail.com>
2021-03-31 12:44:22 +0200

    update the js

---
+++ js-src/NewsletterHandler.js
@@ -0,0 +1,9 @@
+const NewsletterHandler = parent => {
+
+  const init = () => {
+    const closeButton = parent.querySelector('.close-newsletter');
+
+  };
+
+  init();
+};
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-const App = (() => {
+const NewsletterHandler = parent => {
 
   const init = () => {
+    const closeButton = parent.querySelector('.close-newsletter');
+
+  };
+
+  init();
+};;const App = (() => {
+
+  const init = () => {
+    const body = document.body;
     // const header = document.querySelector('.layout-header');
     // const imageContainer = document.querySelectorAll('.image-container');
     // const carouselSlider = document.querySelector('.carousel-slider');
+    const newsletter = document.querySelector('.newsletter');
 
     // if(header) {
     //   HeaderHandler(header, document.body);
@@ -14,6 +24,10 @@ const App = (() => {
     //     LazyLoad(imageContainer[i]);
     //   }
     // }
+
+    if(newsletter) {
+      NewsletterHandler(newsletter);
+    }
 
     // if(carouselSlider) {
     //   CarouselSlider(carouselSlider);
